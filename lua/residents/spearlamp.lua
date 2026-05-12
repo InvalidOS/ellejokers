065e2b72c9a8c6561ddfc65a339de672c762d740
ellestuff. <ellef1566@gmail.com>
2026-05-12 16:50:59 +0100

    lamp sprite fix

--- lua/residents/spearlamp.lua
+++ lua/residents/spearlamp.lua
@@ -51,8 +51,9 @@ ellejokers.Resident {
 	set_ability = function(self, card, initial, delay_sprites)
 		-- 1 in 5 chance of silly lamp
 		if pseudorandom("elle_do_lamp_tf",1,5)==1 then card.ability.extra.variant = pseudorandom("elle_lamp_tf",1,#ellejokers.lamps)+1 end
-		card.children.center:set_sprite_pos({x = card.ability.extra.variant-1, y = ellejokers.mod_data.config.nsfw and 1 or 0})
-	end,
+		if not slimeutils.card_obscured(card) then
+			card.children.center:set_sprite_pos({x = card.ability.extra.variant-1, y = ellejokers.mod_data.config.nsfw and 1 or 0})
+	end end,
 	add_to_deck = function(self, card, from_debuff)
 		if card.ability.extra.variant ~= 1 then check_for_unlock({type = "elle_lamp"}) end
 	end,
